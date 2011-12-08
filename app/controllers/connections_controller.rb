41f6d4079344031d2212149b3d6844697c2f738c
Martin Moen Wulffeld <martin@wulffeld.org>
2011-12-08 23:04:59 +0100

    Correct way of authorizing access.

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -4,7 +4,7 @@ class ConnectionsController < ApplicationController
     @linked_in_client = LinkedIn::Client.new(Mong::Application.config.linkedin_key, Mong::Application.config.linkedin_secret)
     
     # It crashes here.
-    @linked_in_client.authorize_from_request(@linked_in_client.request_token.token, @linked_in_client.request_token.secret, authentication.oauth_verifier)
+    @linked_in_client.authorize_from_access(authentication.token, authentication.secret)
     @linked_in_client
   end
 end
