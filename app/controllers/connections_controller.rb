07c438dad7c1e920ff9569de5ce7ab8a5a1bd910
Martin Moen Wulffeld <martin@wulffeld.org>
2011-12-11 11:58:21 +0100

    Remove comment. It no longer crashes.

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -2,8 +2,6 @@ class ConnectionsController < ApplicationController
   def show
     authentication = current_user.authentications.first
     @linked_in_client = LinkedIn::Client.new(Mong::Application.config.linkedin_key, Mong::Application.config.linkedin_secret)
-    
-    # It crashes here.
     @linked_in_client.authorize_from_access(authentication.token, authentication.secret)
     @linked_in_client
   end
